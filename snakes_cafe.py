6316fdbeb78f20459e2fd29bcceb2789695e1d56
Steve Ngo <stevehngo@hotmail.com>
2022-04-19 20:44:14 -0400

    finish lab01

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -9,30 +9,51 @@ header = [
     '** To quit at any time, type "quit" **',
     stars
 ]
-footer = [strs, "** What would you like to order? **", strs, ">"]
+footer = [strs, "** What would you like to order? **", strs]
 
-Appetizers = ["Wings", "Cookies", "Spring Rolls"]
-Entrees = ["Salmon", "Steak", "Meat Tornado", "A Literal Garden"]
-Desserts = ["Ice Cream", "Cake", "Pie"]
-Drinks = ["Coffee", "Tea", "Unicorn Tears"]
+Appetizers = {"Wings": 0, "Cookies": 0, "Spring Rolls": 0}
+Entrees = {"Salmon": 0, "Steak": 0, "Meat Tornado": 0, "A Literal Garden": 0}
+Desserts = {"Ice Cream": 0, "Cake": 0, "Pie": 0}
+Drinks = {"Coffee": 0, "Tea": 0, "Unicorn Tears": 0}
+
+menu = [Appetizers, Entrees, Desserts, Drinks]
 
 def hf(section):
+    print("")
     for i in section:
         print(i)
-    print("")
 
-def menu_items(list):
-    lnames = [name for name in globals() if globals()[name] is list]
-    print(lnames)
-    print("----------")
-    for x in list:
-        print(x)
+def menu_items(dictionary):
     print("")
+    lnames = [name for name in globals() if globals()[name] is dictionary]
+    print(lnames[0])
+    print("----------")
+    for i in dictionary.keys():
+        print(i)
+
+def menu_gen(list):
+    for i in list:
+        menu_items(i)
+
+def item_search(list, inp):
+    while True:
+        if inp == "quit":
+            break
+        for i in list:
+            if(inp in i):
+                i[inp] = i.get(inp) + 1
+                print(str(i.get(inp)) + " orders of " + inp + " have been added to your meal")
+                user_input(menu)
+        break
+
+
+def user_input(menu):
+    order = input("> ")
+    item_search(menu, order)
+
 
 
 hf(header)
-menu_items(Appetizers)
-menu_items(Entrees)
-menu_items(Desserts)
-menu_items(Drinks)
+menu_gen(menu)
 hf(footer)
+user_input(menu)
